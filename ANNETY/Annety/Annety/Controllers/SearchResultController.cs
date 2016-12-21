b4d5da66c66a19c7273d1952fcadcc0104792751
cohenefrat <efrat335@gmail.com>
2016-12-21 22:56:30 +0200

    more work on searchresult

--- ANNETY/Annety/Annety/Controllers/SearchResultController.cs
+++ ANNETY/Annety/Annety/Controllers/SearchResultController.cs
@@ -8,6 +8,8 @@ namespace Annety.Controllers
 {
     public class SearchResultController : Controller
     {
+        private AnnetyEntities db = new AnnetyEntities();
+
         // GET: SearchResult
         public ActionResult Index()
         {
@@ -16,79 +18,10 @@ namespace Annety.Controllers
 
         public ActionResult FromMenu(int CategoryCode)
         {
+            var products = db.Product.Where(p => p.CategoryCode == CategoryCode).ToList();
             return RedirectToAction("Index");
-        }
 
-        // GET: SearchResult/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
-        // GET: SearchResult/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: SearchResult/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: SearchResult/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: SearchResult/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: SearchResult/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: SearchResult/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
