d48de403500a6096cf206d8f9dd8e5847c77a92d
DanaMazor <Dana834@gmail.com>
2017-01-26 23:59:52 +0200

    Dana 26.1

--- ANNETY/Annety/Annety/Controllers/SearchResultController.cs
+++ ANNETY/Annety/Annety/Controllers/SearchResultController.cs
@@ -21,16 +21,39 @@ namespace Annety.Controllers
         {
             var products = db.Product.Where(p => p.CategoryCode == CategoryCode).ToList();
             return View(products.ToList());
-      
+
 
         }
-        public ActionResult SearchBox(String SearchKey)
+        //add another parameter which symbol the source
+        [HttpGet]
+        public ActionResult Search_Box(String Search_Box)
         {
-            String[] splited = SearchKey.split("\\s+");
+            var products = db.Product.ToList();
+            string a = "Dress";
+            char[] delimiterChars = { ' ', ',', ':', '.' };
+            String[] splited = a.Split(delimiterChars);
+            int[] array = new int[products.Count ];
+            foreach (string s in  splited)
+            {
+                products.Where(u=>u.Desc.Contains(s)).ToList().ForEach(p=>array[products.IndexOf(p)]++);
+                //foreach (var item in products   )
+                //{
+                //    if (item.Desc.Contains(s))
+                //        array[products.IndexOf(item)]++;
+                //}
+            }
+            if (Session["kk"]==null )
+                Session["kk"] = new Stack <Product>();
 
-            return View();
+            Stack <Product> f = (Stack <Product>)Session["kk"];
+            //f.Push();
+            
+                var max = array.Max();
+          //select the products according the array
+
+            return View(products.ToList());
 
 
+            }
         }
-    }
 }
---
+++ ANNETY/Annety/Annety/Extentions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Annety
+{
+    public partial class Categories
+    {
+       
+        public string  LongName
+        {
+            get
+            {
+                string girlOrChild = "";
+                if (this.ParentCategory)
+                    girlOrChild = "girls";
+                else
+                    girlOrChild = "boys";
+
+                return this.Desc+" "+girlOrChild  ;
+
+            }
+         }
+
+    }
+    //public partial class Users
+    //{
+ 
+    //    public string DisplayPassword
+    //    {
+    //      //  get { return Password .decript(); }
+    //        set { Password  = value.להצפין(); }
+    //    }
+
+    //}
+
+
+}
