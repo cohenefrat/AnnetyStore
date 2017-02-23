3b2c60ac28052704aa90cce1bfd965ab2cd439e3
cohenefrat <efrat335@gmail.com>
2017-02-23 17:52:55 +0200

    Efrat 23/2

--- ANNETY/Annety/Annety/Controllers/HomeController.cs
+++ ANNETY/Annety/Annety/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,8 @@ namespace Annety.Controllers
 {
     public class HomeController : Controller
     {
+        private AnnetyEntities db = new AnnetyEntities();
+        
         public ActionResult Index()
         {
             return View();
@@ -33,6 +36,36 @@ namespace Annety.Controllers
 
             return View();
         }
-       
+
+
+        public ActionResult EditDetails()
+        {
+            Users user = new Users();
+            int usercode;
+            if (Session["UserCode"] != null)
+            {
+                usercode = (int)Session["UserCode"];
+                user = db.Users.FirstOrDefault(u => u.Code == usercode);
+                user.Password = null;
+            }
+            return View("UserDetails", user);
+        }
+
+        [HttpPost]
+        public ActionResult EditDetails(Users user)
+        {
+            Users u = new Users();
+            u = user;
+            u.Password = AccountController.HashPass(user.Password);
+            if (ModelState.IsValid)
+            {   
+                db.Entry(u).State = EntityState.Modified;
+                db.SaveChanges();
+                return View("UserDetails", u);
+            }
+
+            return View("UserDetails");
+        }
+
     }
 }
